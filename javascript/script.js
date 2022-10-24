72f81c3b2da7ca71193806f63336fccb2e42be2c
Oduwole Faruq Adetola <oduwoleadetohlar@gmail.com>
2022-10-24 14:21:52 -0700

    Javascript updated

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,15 @@
+// import {app, analytics} from './modules';
+
+// console.log(app);
+
+import { getStorage, ref } from "firebase/storage";
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+const storage = getStorage();
+
+// Create a storage reference from our storage service
+const storageRef = ref(storage);
+
 // GET COUNT ID
 let countEl = document.getElementById("count-el");
 
